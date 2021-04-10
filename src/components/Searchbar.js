d139e04bfb8494af10b6299caa26099c9b70c8fc
Sundarm-Soni <sundarm.soni097@gmail.com>
2021-04-10 11:59:16 +0530

    Styling for friend list done

---
+++ src/components/Searchbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Searchbar = () => {
+    return (
+        <>
+            <input type="text" name="searchbar" placeholder="Enter your friend's name" id="search"/>
+        </>
+    )
+}
+
+export default Searchbar
